b8d172f89bfbccc37fc647c3be78fc04d23ccca3
ShowYourseld <malicrazy03@gmail.com>
2025-06-25 22:50:49 +0300

    Update server.py

--- projects/chat app/server.py
+++ projects/chat app/server.py
@@ -4,34 +4,37 @@ import threading
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('127.0.0.1', 12345))
 server.listen()
-print('server is listing...')
+print('server is listening...')
 
 clients = []
 usernames = []
 
 def broadcast(message, sender=None):
     for client in clients:
-        if client != sender:
             client.send(message)
 
-def handle_client (client):
+def handle_client(client):
     while True:
         try:
-            message = client.recv(1024).encode()
+            message = client.recv(1024)
+            if not message:
+                break  # Client disconnected
             broadcast(message, client)
         except:
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            username = usernames[index]
-            broadcast(f'user {username} has left the chat', client)
-            usernames.remove(username)
             break
+    # Handle disconnect
+    if client in clients:
+        index = clients.index(client)
+        username = usernames[index]
+        broadcast(f'user {username} has left the chat'.encode())
+        clients.remove(client)
+        usernames.remove(username)
+        client.close()
 
 def receive():
     while True:
-        client, addres = server.accept()
-        print(F'conneted with{addres}')
+        client, address = server.accept()
+        print(f'Connected with {address}')
         client.send('USERNAME'.encode())
         username = client.recv(1024).decode()
         usernames.append(username)
@@ -39,13 +42,11 @@ def receive():
         print(f"Username of the client is {username}")
         broadcast(f"{username} joined the chat!".encode(), client)
         client.send("You are now connected!".encode())
-        print(client.recv(1024).decode())
 
         thread = threading.Thread(target=handle_client, args=(client,))
         thread.start()
 
 receive()
-
 
 
 
